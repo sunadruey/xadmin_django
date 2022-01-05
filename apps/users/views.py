7fc0ab4f509461cd47fe3cd50fd672b6d779d967
ShuangmeiZhao <sunadruey@163.com>
2022-01-06 00:25:28 +0800

    登录失败报错

--- apps/users/views.py
+++ apps/users/views.py
@@ -26,7 +26,7 @@ def user_ogin(request):
             login(request, user)
             return render(request, 'index.html')
         else:
-            return render(request, 'index.html', {})
+            return render(request, 'login.html', {"msg": "用户名或密码错误!"})
     elif request.method == "GET":
         return render(request, 'login.html', {})
 
