7f5fa71457a19a70d0104cebfc09bda00c1fc242
ShuangmeiZhao <sunadruey@163.com>
2022-01-06 00:15:37 +0800

    邮箱登录

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.backends import ModelBackend
 from .models import UserProfile
+from django.db.models import Q
 # Create your views here.
 
 
@@ -9,7 +10,7 @@ class CustomBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         # 自己的后台逻辑
         try:
-            user = UserProfile.objects.get(username=username)
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
             if user.check_password(password):
                 return user
         except Exception as e:
