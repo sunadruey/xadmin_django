220cde2d505f9294ec5ec5c6be2b2d8727a34bda
ShuangmeiZhao <sunadruey@163.com>
2022-01-27 22:54:34 +0800

    手机号的验证

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from operation.models import UserAsk
-
+import re
 
 # class UseAskForm(forms.Form):
 #     name = forms.CharField(required=True, max_length=20, min_length=2)
@@ -16,3 +16,22 @@ class UseAskForm(forms.ModelForm):
     class Meta:
         model = UserAsk
         fields = ['name', 'mobile', 'course_name']
+
+    # 自己定义字段的校验规则，命名规则clean开头  后面跟字段
+    def clean_mobile(self):
+        """
+        验证手机号号码是否非法
+        :return:
+        """
+        mobile = self.cleaned_data['mobile']
+        REGEX_MOBILE = "^1[35678]\d{9}$"
+        p = re.compile(REGEX_MOBILE)
+        if p.match(mobile):
+            return mobile
+        else:
+            raise forms.ValidationError(u'手机号码非法', code='mobile_invalid')
+
+
+
+
+
